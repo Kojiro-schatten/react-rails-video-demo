2726c7c154a710c777a968efd8021db93bcc2b97
user <user@user-no-MacBook-Pro.local>
2020-05-14 18:15:59 +0900

    completed search component with API

--- front/src/Components/ApiRender.jsx
+++ front/src/Components/ApiRender.jsx
@@ -4,20 +4,37 @@ import { StyledApiRender } from './styles/StyledApiRender'
  
 function ApiRender() {
   const [data, setData] = useState({ hits: [] });
- 
+  const [query, setQuery] = useState('redux')
+  const [url, setUrl] = useState(
+    'http://hn.algolia.com/api/v1/search?query=redux',   
+  );
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        'https://hn.algolia.com/api/v1/search?query=redux',
-        );
+      const result = await axios(url);
+
       setData(result.data);
     } 
     // fetchDataでfetchを忘れないように...
     fetchData();
-  }, []);
+    // 副作用は search に依存させる
+  }, [url]);
  
   return (
     <StyledApiRender>
+      <input 
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      {/* 副作用はインプットフィールドで一文字入力するたびに更新される query state ではなく search state に依存させる */}
+      <button 
+        type="button" 
+        onClick={() => 
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
+      }>
+        Search
+      </button>
       <ul>
         {data.hits.map(item => (
           <li className="ApiRenderItem" key={item.objectID}>
